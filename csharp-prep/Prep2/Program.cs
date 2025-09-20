7251f205a7b3675b3a064fc128c82a6dce17c6f7
Joseph Clinger <jaclinger@gmail.com>
2025-09-20 16:04:36 -0600

    Prep 1 and 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What grade percentage did you get in class?");
+        string gradeInput = Console.ReadLine();
+        int gradePercentage = int.Parse(gradeInput);
+        string grade = "";
+
+        if (gradePercentage >= 90)
+        {
+            grade = "A";
+        }
+        else if (gradePercentage >= 80)
+        {
+            grade = "B";
+        }
+        else if (gradePercentage >= 70)
+        {
+            grade = "C";
+        }
+        else if (gradePercentage >= 60)
+        {
+            grade = "D";
+        }
+        else
+        {
+            grade = "F";
+        }
+        Console.WriteLine($"Your grade is: {grade}");   
     }
 }
