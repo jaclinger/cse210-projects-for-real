f62d8fb1598eff064b71cdaa90543b944e7b00c9
Joseph Clinger <jaclinger@gmail.com>
2025-10-08 20:07:36 -0600

    Journal

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -4,6 +4,57 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
+        Journal journal = new Journal();
+        PromptGenerator generator = new PromptGenerator();
+
+        int choice = 0;
+
+        while (choice != 5)
+        {
+            Console.WriteLine("Please select one of the following choices:");
+            Console.WriteLine("1. Write a new entry");
+            Console.WriteLine("2. Display the journal");
+            Console.WriteLine("3. Save the journal to a file");
+            Console.WriteLine("4. Load the journal from a file");
+            Console.WriteLine("5. Quit");
+            Console.Write("What would you like to do? ");
+
+            choice = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            if (choice == 1)
+            {
+                string prompt = generator.GetRandomPrompt();
+                Console.WriteLine(prompt);
+                Console.Write("> ");
+                string response = Console.ReadLine();
+
+                string date = DateTime.Now.ToShortDateString();
+                Entry entry = new Entry(date, prompt, response);
+                journal.AddEntry(entry);
+            }
+            else if (choice == 2)
+            {
+                journal.DisplayAll();
+            }
+            else if (choice == 3)
+            {
+                Console.Write("Enter filename to save to: ");
+                string filename = Console.ReadLine();
+                journal.SaveToFile(filename);
+                Console.WriteLine("Journal saved.\n");
+            }
+            else if (choice == 4)
+            {
+                Console.Write("Enter filename to load from: ");
+                string filename = Console.ReadLine();
+                journal.LoadFromFile(filename);
+                Console.WriteLine("Journal loaded.\n");
+            }
+            else if (choice == 5)
+            {
+                Console.WriteLine("Goodbye!");
+            }
+        }
     }
 }
